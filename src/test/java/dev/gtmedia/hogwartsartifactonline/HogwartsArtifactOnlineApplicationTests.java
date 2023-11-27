2eaf83ecfb1fb7566478a7ab49184aa0db96ba77
gerald.taal <41109298+japayuki@users.noreply.github.com>
2023-11-27 09:02:36 +0100

    first commit

---
+++ src/test/java/dev/gtmedia/hogwartsartifactonline/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gtmedia.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
